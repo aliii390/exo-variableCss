676216d7aa360dd21dec449f37347cb7e8f4eaf5
aliii390 <alli.said.errahmani@gmail.com>
2024-11-22 11:21:09 +0100

    ADD: fin de l'exo2

---
+++ exo2/script.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("chooser");
+const root = document.documentElement;
+
+
+
+input.addEventListener("input", handleInput);
+
+
+
+function handleInput(event){
+ root.style.setProperty('--bg-color', event.target.value);
+
+}
+
